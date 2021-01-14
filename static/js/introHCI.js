e63f3967195c846ab5742ed1880307dfbc6804ab
Gabriel Nicolas G. Aganon <gaganon@ucsd.edu>
2021-01-14 10:07:56 -0800

    Added function projectClick to introHCI.js

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -6,4 +6,14 @@ function initializePage() {
     $('#testjs').click(function(e) {
             $('.jumbotron h1').text("Javascript is connected");
             });
+    $("a.thumbnail").click(projectClick);
+}
+
+function projectClick(e) {
+  // Prevents the page from reloading
+  e.preventDefault();
+  /*
+   * In an event handler, $(this) refers to the object that triggers an event!
+   */
+  $(this).css("background-color", "#7fff00");
 }
